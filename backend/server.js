910d2ebc49ddfeaeebabe30cce49856813e032dc
hridhykk <150917485+hridhykk@users.noreply.github.com>
2025-02-04 17:26:45 +0530

    COMMIT

--- backend/server.js
+++ backend/server.js
@@ -21,11 +21,11 @@ cloudinary.config({
 
 const app = express();
 const PORT = process.env.PORT || 5000;
- const __dirname = path.resolve();
 
-app.use(express.json({ limit: "5mb" })); // to parse req.body
-// limit shouldn't be too high to prevent DOS
-app.use(express.urlencoded({ extended: true })); // to parse form data(urlencoded)
+
+app.use(express.json({ limit: "5mb" }));
+
+app.use(express.urlencoded({ extended: true })); 
 
 app.use(cookieParser());
 
@@ -34,13 +34,7 @@ app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/notifications", notificationRoutes);
 
-// if (process.env.NODE_ENV === "production") {
-// 	app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-// 	app.get("*", (req, res) => {
-// 		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-// 	});
-// }
 
 app.listen(PORT, () => {
 	console.log(`Server is running on port ${PORT}`);
